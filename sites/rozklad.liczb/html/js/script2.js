2320317ed097466f0c0f2ea4350fc16fcf2292ae
FenixC4 <43726835+FenixC4@users.noreply.github.com>
2019-03-29 23:44:24 +0100

    Add files via upload

---
+++ sites/rozklad.liczb/html/js/script2.js
@@ -0,0 +1,74 @@
+function myFunction() {
+    var inputNumber = document.getElementById("number").value;
+    var outputNumber = 0;
+    var divider = 0
+
+    for (var limit = 1; limit <= 200; limit++) {
+        var a = false;
+        for (var i = 2; i <= limit; i++) {
+           if (limit%i===0 && i!==limit) {
+              a = true;
+           };
+        };
+        if (a === false, limit != 1) {
+           divider = limit;
+        };
+        if (inputNumber % divider === 0) {
+            outputNumber = inputNumber/divider;
+            let newElement1 = document.createElement("p");
+            newElement1.innerHTML = inputNumber+" | "+divider;
+            document.body.appendChild(newElement1);
+            let newElement = document.createElement("p");
+            newElement.className = "outputs";
+            newElement.innerHTML = outputNumber;
+            document.body.appendChild(newElement);
+            checker(outputNumber)
+            changer()
+            break
+        };
+    };
+    function checker(number) {
+        for (var limit = 1; limit <= 200; limit++) {
+            var a = false;
+            for (var i = 2; i <= limit; i++) {
+               if (limit%i===0 && i!==limit) {
+                  a = true;
+               };
+            };
+            if (a === false, limit != 1) {
+               divider = limit;
+            };
+            if (number % divider === 0) {
+                outputNumber = number/divider;
+                let newElement = document.createElement("p");
+                newElement.className = "outputs";
+                newElement.innerHTML = outputNumber;
+                document.body.appendChild(newElement);
+                checker(outputNumber)
+                changer()
+                break
+            };
+        };
+    };
+    function changer() {
+        var numbers = document.getElementsByClassName("outputs");
+        var array = Array.from(numbers);
+        Array.from(numbers).forEach(num => {
+            for (var limit = 1; limit <= 200; limit++) {
+                var a = false;
+                for (var i = 2; i <= limit; i++) {
+                   if (limit%i===0 && i!==limit) {
+                      a = true;
+                   };
+                };
+                if (a === false, limit != 1) {
+                   divider = limit;
+                };
+                if (num.innerHTML % divider === 0) {
+                    num.innerHTML = num.innerHTML + " | " + divider;
+                    break
+                };
+            };
+        });
+    };
+};
