14d41009470a18b1b0dc38e3bace992fe83b624c
FenixC4 <43726835+FenixC4@users.noreply.github.com>
2019-04-07 00:01:25 +0200

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,10 +1,6 @@
-document.getElementById("button").addEventListener("mousedown", myFunction);
-document.getElementById("button").addEventListener("mouseup", change);
-
 function myFunction() {
-    document.getElementById("button").src = "img/button_pressed.png";
-};
-function change() {
-    document.getElementById("button").src = "img/button.png";
-    document.location.href = "sites/eastereggpage/html/index.html";
-};
+    const input = document.getElementById("haksy_input");
+    if(input.value=="PYRA") {
+        document.location.href="sites/eastereggpage/html/index.html";
+    }
+}
